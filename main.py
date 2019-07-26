94c0ea13a400cfb0e57e8cc80d2d6efc45187ab5
apstemmer <apstemmer@hotmail.com>
2019-07-26 20:12:15 +0100

    Create config

--- main.py
+++ main.py
@@ -1,69 +1,15 @@
 from risk import Game
+from config import config
 
 
 def main():
     # Main control loop of Risk
     # https://www.alternatehistory.com/forum/threads/risk%C2%AE-maps.66855/
-    config = {
-        "NAmerica": [
-            "Alaska",
-            "NWTerritory",
-            "Alberta",
-            "Ontario",
-            "Quebec",
-            "Greenland",
-            "WUSA",
-            "EUSA",
-            "CAmerica"
-        ],
-        "SAmerica": [
-            "Venezuela",
-            "Peru",
-            "Brazil",
-            "Argentina"
-        ],
-        "Europe": [
-            "Iceland",
-            "GBritain",
-            "Scandinavia",
-            "NEurope",
-            "WEurope",
-            "SEurope",
-            "Ukraine"
-        ],
-        "Africa": [
-            "NAfrica",
-            "Egypt",
-            "Congo",
-            "EAfrica",
-            "Madagascar",
-            "SAfrica"
-        ],
-        "Asia": [
-            "Afghanistan",
-            "MEast",
-            "India",
-            "Ural",
-            "China",
-            "Siberia",
-            "Yakutsk",
-            "Irkutsk",
-            "Mongolia",
-            "Siam",
-            "Kamchatka",
-            "Japan"
-        ],
-        "Australia": [
-            "Indonesia",
-            "NGuinea",
-            "EAustralia",
-            "WAustralia"
-        ]
 
-    }
     # Init Game
     # Place troops
     # Attack
     # Attack: NWTerr -> Alaska
     ###
-    g = Game()
+
+    g = Game(config,)
